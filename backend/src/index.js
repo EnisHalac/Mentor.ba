bd66634483d6532c4dfedea648840e99159a5fc7
Enis Halac <enishalac@Eniss-MacBook-Pro.local>
2026-01-27 13:01:15 +0100

    postgresql conect

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import {prisma} from "./prisma.js";
 
 dotenv.config();
 
@@ -12,3 +13,8 @@ app.get("/health", (req, res) => res.json({ ok: true, app: "Mentor.ba" }));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`));
+
+app.get("/db-test", async (req, res) => {
+    const userCount = await prisma.user.count();
+    res.json({ ok : true ,userCount });
+});
