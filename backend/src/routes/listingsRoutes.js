ecb577dde51dfa2c00af0bd672819d27053aa7a9
Enis Halac <enishalac@Eniss-MacBook-Pro.local>
2026-01-29 10:48:20 +0100

    backend - delete listings

--- backend/src/routes/listingsRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getListings, postListing } from "../controllers/listingsController.js";
-
-const router = Router();
-
-router.get("/", getListings);
-router.post("/", postListing);
-
-export default router;
